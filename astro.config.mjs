359965975cf9d3a02f7d2f1dc49c54e4b1dd8c03
BETRAN <10231023@student.itk.ac.id>
2026-04-12 00:54:30 +0800

    Update Bug the  Vercel not know astro.js not not include site porperty

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://modapk-git-main-piz4rroos-projects.vercel.app',
   output: 'server',
   adapter: vercel(),
   vite: {
